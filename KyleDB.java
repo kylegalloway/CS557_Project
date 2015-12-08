73de7d1a7ae705157eaea14c15fe8de4510853af
Kyle <ckgalloway@crimson.ua.edu>
2015-12-08 10:55:11 -0600

    Change to OO (sort of)

--- KyleDB.java
+++ KyleDB.java
@@ -2,34 +2,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.IllegalStateException;
 import java.lang.Integer;
-import java.util.NoSuchElementException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.lang.StringIndexOutOfBoundsException;
+import java.util.*;
 
 public class KyleDB {
-    private static Map<Integer, Map<String,String>> storage = new HashMap<Integer, Map<String,String>>();
+    private static ArrayList<Document> storage = new ArrayList<Document>();
+
     public static void main(String[] args) {
         if (args.length == 1) {
             try {
                 Scanner input = new Scanner(new File(args[0]));
-                Integer id = 1;
                 while (input.hasNext()) {
-                    Map<String,String> tempMap = new HashMap<String,String>();
                     Scanner line = new Scanner(input.nextLine()).useDelimiter("\\W+");
+                    Document doc = new Document();
                     while (line.hasNext()) {
                         String next = line.next();
                         next = next.substring(0, next.length());
-                        tempMap.put(next,line.next());
+                        doc.addPair(new KVpair(next,line.next()));
                     }
-                    storage.put(id, tempMap);
-                    id++;
+                    storage.add(doc);
                 }
-                // for(Integer key: storage.keySet()) {
-                //     for(String s: storage.get(key).keySet()) {
-                //         System.out.printf("%d\t%s: %s\n", key, s, storage.get(key).get(s));
-                //     }
-                // }
 
                 if (input != null) { input.close(); }
             }
@@ -46,54 +38,88 @@ public class KyleDB {
         Scanner listener = new Scanner(System.in);
         boolean endflag = false;
         while(!(endflag)) {
-            String user_input = listener.next();
-            if (user_input.equals("help")) {
-                System.out.printf("\nHelp is on it's way!\n\nTo query the database please use the query command:\n\ndb.CS557.query(condition, field)\n\ncondition -- field_name op value\n    zero or more select conditions. If there is more than one condition they will be separated by an 'and' ('or' will not be included). Zero conditions are denoted with just a comma and a space before the field, and it means include all documents e.g. (, field).\n\nop can be =, <, >\n\nfield -- field_name(s)\n    zero or more field names separated by '+' signs. This operation specifies the list of fields to be displayed.  If there are zero fields, the closing parenthesis is used and it means include all fields for each document that satisfies the condition, including the ID field.  Unlike Mongo DB, the ID field is only included if specified.  If a document does not have a field in the field_name list, but it has other fields appearing in the list, then it should be included in the result.\n\nquery() returns the entire collection, all fields for all documents\n\nExample queries and their results:\n    db.CS557.query(Manager = 555, SNum+Dept)\n    Dept: 10  SNum: 777\n    SNum: 222\n    db.CS557.query(Age > 15 and Manager = 555)\n    ID: 2 Dept: 10  Manager:  555 SNum: 777 Age: 20\n    db.CS557.query(, ID+SNum)\n    ID: 1 SNum: 555\n    ID: 2 SNum: 777\n    ID: 3 SNum: 888\n    ID: 4 SNum: 222\n\nTo use an aggregate query (sum, max, avg):\n    aggregate(field)\n\nExample aggregate queries and their results:\n    db.CS557.sum(Age)\n    48\n    db.CS557.max(SNum)\n    888\n    db.CS557.avg(Age)\n    16\n\nTo get the Cartesian product of two fields:\n    cartprod(f1, f2)\n\nExample of Cartesian product query and its result:\n    db.CS557.cartprod(Dept, Age)\n    DEPT: 5 Age: 20\n    DEPT: 5 Age: 18\n    DEPT: 5 Age: 10\n    DEPT: 10 Age: 20\n    DEPT: 10 Age: 18\n    DEPT: 10 Age: 10\n\n>> ");
-            } else if(user_input.equals("quit")) {
-                endflag = true;
-            } else if(user_input.startsWith("db.CS557.query")) {
-                System.out.printf("Query\n\n>> ");
-            } else if(user_input.startsWith("db.CS557.max")) {
-                int max = Integer.MIN_VALUE;
-                String field = user_input.substring(13, user_input.length() - 1);
-                for(Integer key: storage.keySet()) {
-                    if(storage.get(key).containsKey(field)) {
-                        int value = Integer.valueOf(storage.get(key).get(field));
-                        if(value > max) {
-                            max = value;
+            try {
+                String user_input = listener.next();
+                if (user_input.toLowerCase().equals("help".toLowerCase())) {
+                    System.out.printf("\nHelp is on it's way!\n\nTo query the database please use the query command:\n\ndb.CS557.query(condition, field)\n\ncondition -- field_name op value\n    zero or more select conditions. If there is more than one condition they will be separated by an 'and' ('or' will not be included). Zero conditions are denoted with just a comma and a space before the field, and it means include all documents e.g. (, field).\n\nop can be =, <, >\n\nfield -- field_name(s)\n    zero or more field names separated by '+' signs. This operation specifies the list of fields to be displayed.  If there are zero fields, the closing parenthesis is used and it means include all fields for each document that satisfies the condition, including the ID field.  Unlike Mongo DB, the ID field is only included if specified.  If a document does not have a field in the field_name list, but it has other fields appearing in the list, then it should be included in the result.\n\nquery() returns the entire collection, all fields for all documents\n\nExample queries and their results:\n    db.CS557.query(Manager = 555, SNum+Dept)\n    Dept: 10  SNum: 777\n    SNum: 222\n    db.CS557.query(Age > 15 and Manager = 555)\n    ID: 2 Dept: 10  Manager:  555 SNum: 777 Age: 20\n    db.CS557.query(, ID+SNum)\n    ID: 1 SNum: 555\n    ID: 2 SNum: 777\n    ID: 3 SNum: 888\n    ID: 4 SNum: 222\n\nTo use an aggregate query (sum, max, avg):\n    aggregate(field)\n\nExample aggregate queries and their results:\n    db.CS557.sum(Age)\n    48\n    db.CS557.max(SNum)\n    888\n    db.CS557.avg(Age)\n    16\n\nTo get the Cartesian product of two fields:\n    cartprod(f1, f2)\n\nExample of Cartesian product query and its result:\n    db.CS557.cartprod(Dept, Age)\n    DEPT: 5 Age: 20\n    DEPT: 5 Age: 18\n    DEPT: 5 Age: 10\n    DEPT: 10 Age: 20\n    DEPT: 10 Age: 18\n    DEPT: 10 Age: 10\n\n>> ");
+                } else if(user_input.toLowerCase().equals("quit".toLowerCase())) {
+                    endflag = true;
+                } else if(user_input.toLowerCase().equals("show".toLowerCase())) {
+                    for (int i = 0; i < storage.size(); i++) {
+                        for (int j = 0; j < storage.get(i).size(); j++) {
+                            System.out.printf("%d\t%s: %s\n", i + 1, storage.get(i).get(j).getKey(), storage.get(i).get(j).getValue());
                         }
-
                     }
-                }
-                if(max > Integer.MIN_VALUE) {System.out.printf("%d\n\n>> ",max);}
-                else {System.out.printf("\n>> ");}
-            } else if(user_input.startsWith("db.CS557.avg")) {
-                int sum = 0;
-                int count = 0;
-                String field = user_input.substring(13, user_input.length() - 1);
-                for(Integer key: storage.keySet()) {
-                    if(storage.get(key).containsKey(field)) {
-                        sum += Integer.valueOf(storage.get(key).get(field));
-                        count += 1;
+                    System.out.printf("\n\n>> ");
+                } else if(user_input.toLowerCase().startsWith("db.CS557.query".toLowerCase())) {
+                    System.out.printf("Query\n\n>> ");
+                } else if(user_input.toLowerCase().startsWith("db.CS557.max".toLowerCase())) {
+                    int max = Integer.MIN_VALUE;
+                    String field = user_input.substring(13, user_input.length() - 1);
+                    for (int i = 0; i < storage.size(); i++) {
+                        for (int j = 0; j < storage.get(i).size(); j++) {
+                            if(storage.get(i).get(j).getKey().toLowerCase().equals(field.toLowerCase())) {
+                                int value = Integer.valueOf(storage.get(i).get(j).getValue());
+                                if(value > max) {
+                                    max = value;
+                                }
+                            }
+                        }
                     }
-                }
-                System.out.printf("%d\n\n>> ",sum/count);
-            } else if(user_input.startsWith("db.CS557.sum")) {
-                int sum = 0;
-                String field = user_input.substring(13, user_input.length() - 1);
-                for(Integer key: storage.keySet()) {
-                    if(storage.get(key).containsKey(field)) {
-                        sum += Integer.valueOf(storage.get(key).get(field));
+                    if(max > Integer.MIN_VALUE) { System.out.printf("%d\n\n>> ",max); }
+                    else { System.out.printf("\n>> "); }
+                } else if(user_input.toLowerCase().startsWith("db.CS557.avg".toLowerCase())) {
+                    int sum = 0;
+                    int count = 0;
+                    String field = user_input.substring(13, user_input.length() - 1);
+                    for (int i = 0; i < storage.size(); i++) {
+                        for (int j = 0; j < storage.get(i).size(); j++) {
+                            if(storage.get(i).get(j).getKey().toLowerCase().equals(field.toLowerCase())) {
+                                    sum += Integer.valueOf(storage.get(i).get(j).getValue());
+                                    count += 1;
+                            }
+                        }
                     }
+                    System.out.printf("%d\n\n>> ",sum/count);
+                } else if(user_input.toLowerCase().startsWith("db.CS557.sum".toLowerCase())) {
+                    int sum = 0;
+                    String field = user_input.substring(13, user_input.length() - 1);
+                    for (int i = 0; i < storage.size(); i++) {
+                        for (int j = 0; j < storage.get(i).size(); j++) {
+                            if(storage.get(i).get(j).getKey().toLowerCase().equals(field.toLowerCase())) {
+                                sum += Integer.valueOf(storage.get(i).get(j).getValue());
+                            }
+                        }
+                    }
+                    System.out.printf("%d\n\n>> ",sum);
+                } else if(user_input.toLowerCase().startsWith("db.CS557.cartprod".toLowerCase())) {
+                    String[] fields = user_input.substring(18, user_input.length() - 1).split(",");
+                    if (fields.length == 2) {
+                        for (int i = 0; i < storage.size(); i++) {
+                            for (int j = 0; j < storage.get(i).size(); j++) {
+                                if(storage.get(i).get(j).getKey().toLowerCase().equals(fields[0].toLowerCase())) {
+                                    for (int k = 0; k < storage.size(); k++) {
+                                        for (int l = 0; l < storage.get(i).size(); l++) {
+                                            if(storage.get(k).get(l).getKey().toLowerCase().equals(fields[1].toLowerCase())) {
+                                                System.out.printf("%s: %s %s: %s\n", storage.get(i).get(j).getKey(), storage.get(i).get(j).getValue(), storage.get(k).get(l).getKey(), storage.get(k).get(l).getValue());
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    System.out.printf("\n>> ");
+                } else if(user_input.toLowerCase().startsWith("db.CS557.sort".toLowerCase())) {
+                    String field = user_input.substring(14, user_input.length() - 1);
+                    System.out.printf("Sort on %s\n\n>> ",field);
+                } else if(user_input.toLowerCase().startsWith("db.CS557.delete".toLowerCase())) {
+                    System.out.printf("Delete\n\n>> ");
+                } else {
+                    System.out.printf("I'm sorry but the command you've entered is not valid. Please try again.\n\n>> ");
                 }
-                System.out.printf("%d\n\n>> ",sum);
-            } else if(user_input.startsWith("db.CS557.cartprod")) {
-                System.out.printf("Cartesian Product\n\n>> ");
-            } else if(user_input.startsWith("db.CS557.sort")) {
-                System.out.printf("Sort\n\n>> ");
-            } else if(user_input.startsWith("db.CS557.delete")) {
-                System.out.printf("Delete\n\n>> ");
-            } else {
+            }
+            catch (StringIndexOutOfBoundsException exce) {
                 System.out.printf("I'm sorry but the command you've entered is not valid. Please try again.\n\n>> ");
             }
         }
@@ -102,5 +128,51 @@ public class KyleDB {
     public static void readfile_error() {
         System.err.println("File improperly formed. Terminating.");
         System.exit(1);
+    }
+}
+
+class Document {
+    private KVpair[] doc_array;
+    private int length;
+
+    public Document()
+    {
+        doc_array = new KVpair[100];
+        length = 0;
+    }
+
+    public void addPair(KVpair kvpair)
+    {
+        if (doc_array.length == length) {
+            KVpair[] newdoc_array = new KVpair[2 * length];
+            for (int i = 0; i < length; i++) { newdoc_array[i] = doc_array[i]; }
+            doc_array = newdoc_array;
+        }
+
+        doc_array[length++] = kvpair;
+    }
+
+    public KVpair get(int i) { return doc_array[i]; }
+    public int size() { return length; }
+}
+
+class KVpair {
+    private String key;
+    private String value;
+
+    // Constructor for Product Class
+    public KVpair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    } // End constructor
+
+    public String getKey() { return key; }
+    public String getValue() { return value; }
+
+    public int compareTo(KVpair otherPair) {
+        if(this.key.toLowerCase().equals(otherPair.getKey().toLowerCase())) {
+            return 1;
+        }
+        return 0;
     }
 }
